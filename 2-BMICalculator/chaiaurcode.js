bb7e51dbfc116b13defe9dee0b8a10fbd51091d3
Nitish-cy <85378246+Nitish-cy@users.noreply.github.com>
2023-08-30 21:33:49 +0530

    BMI js code

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -1,3 +1,19 @@
 const form = document.querySelector('form');
 
-console.log(form);
+//console.log(form);
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const h = parseInt(document.querySelector('#height').value);
+  // console.log(h);
+  const w = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (h === '' || isNaN(h) || h < 0) {
+    results.innerHTML = 'please Enter a valid number';
+  } else if (w === '' || isNaN(w) || w < 0) {
+    results.innerHTML = 'please Enter a valid number';
+  } else {
+    const BMI = (w / ((h * h) / 10000)).toFixed(2);
+    results.innerHTML = BMI;
+  }
+});
